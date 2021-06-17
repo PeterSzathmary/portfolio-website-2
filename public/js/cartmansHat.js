a45a7799e4b29a9d9e7181088133cafb58110082
Peter Szathmary <pet.szathmary@gmail.com>
2021-06-17 18:54:22 +0200

    Added Cartman :D and changing his hat (or cap, I dunno) color in every new visit of page

---
+++ public/js/cartmansHat.js
@@ -0,0 +1,39 @@
+"use strict";
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+var svg = document.getElementById("my-picture");
+console.log("svg", svg);
+// It's important to add an load event listener to the object,
+// as it will load the svg doc asynchronously
+svg.addEventListener("load", function () {
+    // get the inner DOM of alpha.svg
+    var svgDoc = svg.contentDocument;
+    console.log("svgDoc", svgDoc);
+    console.log("svgDoc.all", svgDoc.all);
+    console.log("svgDoc.all[87]", svgDoc.all[87]);
+    console.log("svgDoc.all.length", svgDoc.all.length);
+    // get the inner element by id
+    var colorHat = svgDoc.getElementById("ColorHat");
+    console.log(colorHat);
+    colorHat.setAttribute("style", "fill: rgb(" +
+        getRandomInt(0, 255) +
+        "," +
+        getRandomInt(0, 255) +
+        "," +
+        getRandomInt(0, 255) +
+        ")");
+    // add behaviour
+    colorHat.addEventListener("mousedown", function () {
+        alert("You have clicked on me! Mammaaa!");
+    }, false);
+}, false);
--- withPython.py
+++ withPython.py
@@ -1,17 +1,17 @@
 import requests
 import sys
 
-lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget mi scelerisque, pharetra neque quis, dictum dolor. Sed metus enim, egestas vel ex non, commodo ultricies magna. Pellentesque volutpat dictum libero a ultrices. Mauris et velit in est ornare rhoncus a at neque. Ut ullamcorper gravida vulputate. Praesent sed odio lorem. Nulla tempor est quis odio vehicula, sed tincidunt sapien feugiat. Suspendisse nec magna dolor. Sed id placerat arcu. Phasellus et urna mauris. Donec ligula eros, consequat eget eleifend vitae, convallis eu ex. Donec cursus metus a ipsum viverra gravida. "
+lorem = "Today I have tried some Pandas with python. It's cool!"
 
 if __name__ == "__main__":
     #print(f"Arguments count: {len(sys.argv)}")
-    url = "http://localhost:3000/journals"
+    url = "http://localhost:3000/newjournal"
     if len(sys.argv) > 1:
         print(f"Sending to server {sys.argv[1]}")
         obj = {"content": sys.argv[1]}
     else:
         obj = {"content": lorem}
 
-    for i in range(3):
+    for i in range(1):
         x = requests.post(url, data=obj)
         print(x)
