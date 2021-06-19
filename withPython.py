5665fb806334e0da830d62a7bdc69706b72bda7a
Peter Szathmary <pet.szathmary@gmail.com>
2021-06-19 21:43:34 +0200

    Now it is all working as it should I hope... for now! :)

--- withPython.py
+++ withPython.py
@@ -1,11 +1,16 @@
 import requests
 import sys
 
-lorem = "Idem si kúpiť knihu o anatómii."
+lorem = "This is amazing! I learned something new again from here: https://the.earth.li/~sgtatham/putty/0.60/htmldoc/Chapter5.html"
+
+to_public = True
 
 if __name__ == "__main__":
     #print(f"Arguments count: {len(sys.argv)}")
-    url = "http://18.218.116.226:3000/newjournal"
+    if to_public:
+        url = "http://18.218.116.226:3000/newjournal"
+    else:
+        url = "http://localhost:3000/newjournal"
     if len(sys.argv) > 1:
         print(f"Sending to server {sys.argv[1]}")
         obj = {"content": sys.argv[1]}
